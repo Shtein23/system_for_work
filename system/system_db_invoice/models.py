7237ddff51bb335d609fd3ca696352bed0f7f74c
Evgenii <fyodorov.1997@gmail.com>
2022-04-11 18:24:44 +0300

    тесты шаблонов

--- system/system_db_invoice/models.py
+++ system/system_db_invoice/models.py
@@ -3,23 +3,23 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
-    name = models.CharField()
-    surname = models.CharField()
-
-
-class Product(models.Model):
-    name = models.CharField()
-
-
-class Status(models.Model):
-    name = models.CharField()
-    style_class = models.CharField()
-
-
-class Actual_order(models.Model):
-    number = models.CharField()
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    status = models.CharField(Status, on_delete=models.CASCADE)
+# class User(models.Model):
+#     name = models.CharField()
+#     surname = models.CharField()
+#
+#
+# class Product(models.Model):
+#     name = models.CharField()
+#
+#
+# class Status(models.Model):
+#     name = models.CharField()
+#     style_class = models.CharField()
+#
+#
+# class Actual_order(models.Model):
+#     number = models.CharField()
+#     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     status = models.CharField(Status, on_delete=models.CASCADE)
 
 

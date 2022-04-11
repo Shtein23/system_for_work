b791d67446c2ae62bcc160ab8c1523fc0a47f922
Shtein22 <fyodorov.1997@gmai.com>
2022-04-12 00:03:51 +0300

    check forms

--- system/system_db_invoice/views.py
+++ system/system_db_invoice/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import *
+from .forms import UserForm
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {'form': UserForm()})

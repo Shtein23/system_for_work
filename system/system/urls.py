68576f7fc1ec06c6713d5d9d942b0b8ec4cbafb3
Shtein23 <fyodorov.1997@gmail.com>
2022-04-12 00:33:41 +0300

    test change commit author

--- system/system/urls.py
+++ system/system/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from system_db_invoice import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
-    path('', views.index)
+
 ]
